8784d3d6764b6edf7d8483cfac5b2cb981086691
Atuhaire Brian <bryanatuhaire3@gmail.com>
2025-06-13 12:51:12 +0300

    ew learnt changes to the Laravel project

---
+++ resources/views/aboutus.blade.php
@@ -0,0 +1,7 @@
+@extends('layout')
+@section('title', 'About Us')
+@section('content')
+    <h1>Welcome to the about us page!</h1>
+    <p>This is a simple page.</p>
+    <p>Feel free to explore the features and functionalities.</p>
+    @endsection
---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layout')
+@section('title', 'Home')
+@section('content')
+    <h1>Welcome to the home page!</h1>
+    <p>This is a simple home page.</p>
+    <p>Feel free to explore the features and functionalities.</p>
+    @endsection
